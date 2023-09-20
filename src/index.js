982141506e5582a14a62bd743b70dde940c89537
HuyHo2404 <81478457+HuyHo2404@users.noreply.github.com>
2023-09-20 18:59:56 +0700

    them field search & hieu phuong thuc get - post

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const port = 3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({
+  extended:true,
+}));
+app.use(express.json());
+
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -30,6 +35,13 @@ app.get("/", (req, res) => {
 });
 app.get("/news", (req, res) => {
   res.render("news");
+});
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 // 127.0.0.1 - localhost:3000
